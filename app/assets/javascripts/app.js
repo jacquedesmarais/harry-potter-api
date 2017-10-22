997a4a85ff307861882910d176835346fa48ef8b
“jacquedesmaraisgit config --global user.email “jacquedes@gmail.com <jacquedes@gmail.com>
2017-10-22 10:29:26 -0500

    transitions and ability to delete

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -7,7 +7,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
       newPersonFirstName: "",
       newPersonLastName: "",
       newPersonHouse: "",
-      errors: []
+      errors: [],
+      searchTermFilter: ""
     },
     mounted: function() {
       $.get('/api/v2/characters.json', function(data) {
@@ -37,7 +38,21 @@ document.addEventListener("DOMContentLoaded", function(event) {
       },
 
       deletePerson: function(inputCharacter) {
-        
+        $.ajax({
+          type: 'DELETE',
+          url: `/api/v2/characters/${inputCharacter.id}.json`,
+          contentType: 'application/json',
+          success: function(newCharacter) {
+            this.characters = newCharacter;
+          }.bind(this)
+        });
+      },
+
+      isValidCharacter: function(inputCharacter) {
+        var validFirstName = inputCharacter.first_name.toLowerCase().indexOf(this.searchTermFilter.toLowerCase()) !== -1;
+        var validLastName = inputCharacter.last_name.toLowerCase().indexOf(this.searchTermFilter.toLowerCase()) !== -1;
+        var validHouse = inputCharacter.house.toLowerCase().indexOf(this.searchTermFilter.toLowerCase()) !== -1;
+        return validFirstName || validLastName || validHouse;
       }
     },
     computed: {
